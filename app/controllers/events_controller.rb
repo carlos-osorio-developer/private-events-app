0d16ae8c86a78578120fb3b7a45aba6983b170c2
carlos-osorio-developer <osorio.uis@gmail.com>
2021-05-26 10:48:02 -0500

    Add index view for Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
     def index
-      @users = User.all
+      @events = Event.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
+  resources :events, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  root to: "events#index"
 end
