74ebcb8e030531f886428e9e8ea4ac26fea32ba0
carlos-osorio-developer <osorio.uis@gmail.com>
2021-05-26 15:56:57 -0500

    Add #new and #create methods to the MVC

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,28 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, only: [:new]
     def index
       @events = Event.all
     end
+
+    def new
+      @event = Event.new
+    end
+
+    def create
+      
+      @event = current_user.events.build(event_params)
+
+      if @event.save
+        redirect_to events_path
+      else
+        render :new
+      end
+    end
+    
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :info, :date)
+  end
 end
