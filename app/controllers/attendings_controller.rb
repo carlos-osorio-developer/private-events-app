ab5e2deb8541c586fa313ba74240fe95430b55dd
carlos-osorio-developer <osorio.uis@gmail.com>
2021-05-27 10:11:30 -0500

    Add uniqueness validation for both columns in attendings table

--- app/controllers/attendings_controller.rb
+++ app/controllers/attendings_controller.rb
@@ -2,7 +2,10 @@ class AttendingsController < ApplicationController
   def attend
     @event = Event.find_by_id(params[:id])
     record = current_user.attendings.build(attended_event_id: @event.id)
-    record.save
-    redirect_to @event
+    if record.save
+      redirect_to @event
+    else
+      redirect_to @event, notice: "User already attending"
+    end
   end
 end
