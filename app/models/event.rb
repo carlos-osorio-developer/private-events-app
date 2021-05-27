0e255d040eefa1001849e160f390312ba6b705cc
carlos-osorio-developer <osorio.uis@gmail.com>
2021-05-26 19:54:19 -0500

    Create scopes for future and past events queries

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     has_many :attendings, foreign_key: :attended_event_id
     has_many :attendees, through: :attendings
+
+    scope :past, -> { where("date < '#{Time.now}'") }
+    scope :future, -> { where("date >= '#{Time.now}'") }
 end
